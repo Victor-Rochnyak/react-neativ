5afd97db83aca80ada3decdc73d8f8dd5ee91615
Victor-Rochnyak <victor.rochnyak@gmail.com>
2023-04-15 22:06:14 +0300

    дз2

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { RegistrationScreen } from "./Screens/RegistrationScreen";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World3443rwrwqr24!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <RegistrationScreen />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ constans/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    mainText: "#212121",
+    secondaryText: "rgba(33, 33, 33, 0.8)",
+    accent: "#FF6C00",
+    secondaryAccent: "#1B4371",
+    mainBackground: "#ffffff",
+    inputBackground: "F6F6F6",
+    placeholder: "#BDBDBD",
+    border: "#E8E8E8",
+  },
+};
