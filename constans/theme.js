03c677ef39558fd7283d3881f8158c891227ec7a
Victor-Rochnyak <victor.rochnyak@gmail.com>
2023-04-20 17:38:45 +0300

    Debugger

--- constans/theme.js
+++ constans/theme.js
@@ -5,7 +5,7 @@ export const theme = {
     accent: "#FF6C00",
     secondaryAccent: "#1B4371",
     mainBackground: "#ffffff",
-    inputBackground: "F6F6F6",
+    inputBackground: "#F6F6F6",
     placeholder: "#BDBDBD",
     border: "#E8E8E8",
   },
