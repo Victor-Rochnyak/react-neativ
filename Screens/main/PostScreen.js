eae351457f16186155859385d4dee25dd8f1c76a
Victor-Rochnyak <victor.rochnyak@gmail.com>
2023-04-30 12:59:18 +0300

    додав іконку

--- Screens/main/PostScreen.js
+++ Screens/main/PostScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const PostScreen = () => {
+const PostScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>PostScreen</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Login")}
+        style={{ marginRight: 10 }}
+      >
+        <MaterialCommunityIcons name="logout" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -16,4 +24,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
+
 export default PostScreen;
